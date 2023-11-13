2352f9766e492000ca411bce4ac11f867e1f8593
sarthak sharma <sharma.sarthak222872@gmail.com>
2023-11-13 22:00:07 +0530

    Added the youtube funcnality

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,14 +1,21 @@
+import { auth } from '@/firebase/firebase';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
+import Logout from '../Buttons/Logout';
+import { useRecoilState } from 'recoil';
+import { authModalState } from '@/atoms/authModalsAtom';
 
 type TopbarProps = {};
 
 const Topbar: React.FC<TopbarProps> = () => {
+  const [user] = useState(auth);
+  const [authModal, setAuthModalState] = useRecoilState(authModalState);
+
   return (
     <nav className="relative flex h-[50px] w-full items-center px-5 bg-dark-layer-1 text-dark-gray-7">
-      <div className={`flex w-full items-center justify-between max-w-[1200px] mx-auto`}>
-        <Link href="/" className='h-[22px] flex-1'>
-            <img src="/logo-full.png" alt="Logo" className='h-full' />
+      <div className="flex w-full items-center justify-between max-w-[1200px] mx-auto">
+        <Link href="/" className="h-[22px] flex-1">
+          <img src="/logo-full.png" alt="Logo" className="h-full" />
         </Link>
 
         <div className="flex items-center space-x-4 flex-1 justify-end">
@@ -22,9 +29,25 @@ const Topbar: React.FC<TopbarProps> = () => {
               GitHub
             </a>
           </div>
-          <Link href="/auth">
+          {!user && (
+            <Link
+              href="/auth"
+              onClick={() => setAuthModalState((prev) => ({ ...prev, isOpen: true, type: 'login' }))}
+            >
               <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded">Sign In</button>
-          </Link>
+            </Link>
+          )}
+          {user && (
+            <div className="cursor-pointer group relative">
+              <img src="/avatar.png" alt="User Profile" className="h-8 w-8 rounded-full" />
+              <div
+                className="absolute top-10 left-2/4 -translate-x-2/4 mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg z-40 group-hover:scale-100 scale-0 transition-all duration-300 ease-in-out"
+              >
+                {/* <p className='text-sm'>{user.email}</p> */}
+              </div>
+            </div>
+          )}
+          {user && <Logout />}
         </div>
       </div>
     </nav>
